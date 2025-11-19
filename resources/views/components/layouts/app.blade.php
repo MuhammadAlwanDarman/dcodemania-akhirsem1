b6af2474c115232bd9e4c08b1d54459501ea2dbb
MuhammadAlwanDarman <growtowindo@gmail.com>
2025-11-19 16:27:38 +0800

    feat: implement HomePage, Navbar, and Footer components with Tailwind CSS styling

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <title>{{ $title ?? 'DCodeMania' }}</title>
+        @livewireStyles 
+    </head>
+    <body class="bg-slate-200 dark:bg-slate-700">
+        @livewire('partials.navbar')
+        <main>
+            {{ $slot }}
+        </main>
+        @livewire('partials.footer')
+        @livewireScripts    
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </body>
+</html>

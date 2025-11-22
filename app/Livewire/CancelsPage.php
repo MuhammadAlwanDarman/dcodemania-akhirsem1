21631de857f4cf869261126a5504e55f87f94d4b
MuhammadAlwanDarman <growtowindo@gmail.com>
2025-11-23 07:37:50 +0800

    Add new pages for password reset, order cancellation, checkout, order details, my orders, and success confirmation
    - Implemented reset password page with form validation.
    - Created a cancellation page to inform users of payment failure.
    - Developed a comprehensive checkout page with shipping and payment options.
    - Added order detail page displaying customer information and order summary.
    - Introduced my orders page to list all user orders with status and actions.
    - Designed a success page to confirm order receipt and provide details.

---
+++ app/Livewire/CancelsPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CancelsPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.cancels-page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\CancelsPage;
 use Illuminate\Support\Facades\Route;
 
 use App\Livewire\HomePage;
@@ -15,10 +17,24 @@ use App\Livewire\CancelPage;
 
 use App\Livewire\Auth\LoginPage;
 use App\Livewire\Auth\RegisterPage;
-use App\Livewire\Auth\ForgotPage;
 use App\Livewire\Auth\ResetPasswordPage;
 use App\Livewire\ProductsPage;
 
 Route::get('/', HomePage::class)->name('home');
-Route::get('/categories', CategoriesPage::class)->name('categories');
+Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
+Route::get('/cart', CartPage::class);
+Route::get('/products/{product}',ProductsPage::class)->name('product.detail');
+
+Route::get('/checkout', CheckOutPage::class);
+Route::get('/my-order', MyOrderPage::class);
+Route::get('/my-order/{order}', MyOrderDetailPage::class);
+
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPasswordPage::class);
+Route::get('/forgot', ResetPasswordPage::class);
+Route::get('/reset-password', ResetPasswordPage::class);
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelsPage::class);
